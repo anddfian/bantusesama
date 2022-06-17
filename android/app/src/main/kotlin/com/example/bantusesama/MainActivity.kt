61a6bcf71cc3b0bb97975eee77cf3ba67e8c5744
Andi Alfian Bahtiar <andd.fian@gmail.com>
2022-06-17 21:20:33 +0800

    First Commit

---
+++ android/app/src/main/kotlin/com/example/bantusesama/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.delixha.bantusesama
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:bantusesama/screen/splash_screen.dart';
+import 'package:flutter/material.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:get/get.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
